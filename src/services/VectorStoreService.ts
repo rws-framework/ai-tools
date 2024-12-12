5c7051953ca6767ea96662417b4e4542d1e40b0d
Papa Black <jakpawlik@gmail.com>
2024-12-12 23:06:19 +0100

    v0.0.9

--- src/services/VectorStoreService.ts
+++ src/services/VectorStoreService.ts
@@ -1,5 +1,5 @@
 import { EmbeddingsInterface } from '@langchain/core/embeddings';
-import { Injectable } from '../../nest';  
+import { Injectable } from '@rws-framework/server/nest';  
 
 import RWSVectorStore, { VectorDocType } from '../models/convo/VectorStore';
 
